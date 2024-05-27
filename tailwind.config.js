f16f759816bc43b28f2cddc8c3fb4ea2a269745b
Mateus Frazão <mateusfrazao12345@gmail.com>
2024-05-27 17:24:18 -0300

    Finalização do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Background': 'hsl(0, 0%, 8%)',
+        'green-black':'hsl(75, 94%, 57%)',
+        'button' :'hsl(0, 0%, 20%)',
+        'white-fx':'hsl(0, 0%, 100%)',
+        'dark-page':'hsl(0, 0%, 12%)',
+      },
+      width: {
+        '400': '400px',
+        '120':'120px',
+      },
+      height: {
+        '670': '670px',
+      }
+    },
   },
   plugins: [],
 }
